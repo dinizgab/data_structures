ad0f84d67c86a786847ce8aaf322d9b97a0d4600
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-02-28 10:16:18 -0300

    Adding linked_list DE

---
+++ linked_list.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type List struct {
+	Head *Node
+}
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+func main() {
+	l := NewList()
+	l.InsertLast(20)
+	l.InsertLast(30)
+	l.InsertLast(40)
+	l.InsertFirst(50)
+
+	l.Contains(40)
+	l.Contains(60)
+	l.PrintList()
+}
+
+func NewList() *List {
+	return &List{
+		Head: nil,
+	}
+}
+
+func (l *List) InsertFirst(value int) {
+	insertNode := &Node{
+		Value: value,
+	}
+
+	insertNode.Next = l.Head
+	l.Head = insertNode
+}
+
+func (l *List) InsertLast(value int) {
+	insertNode := &Node{
+		Value: value,
+	}
+
+	if l.Head == nil {
+		l.Head = insertNode
+		return
+	}
+
+	curr := l.Head
+	for curr.Next != nil {
+		curr = curr.Next
+	}
+
+	curr.Next = insertNode
+}
+
+func (l *List) Remove()
+
+func (l *List) IsEmpty() bool {
+	return l.Head == nil
+}
+
+func (l *List) GetFirst() *Node {
+	return l.Head
+}
+
+func (l *List) GetLast() *Node {
+	curr := l.Head
+
+	if !l.IsEmpty() {
+		for curr.Next != nil {
+			curr = curr.Next
+		}
+	}
+
+	return curr
+}
+
+func (l *List) GetIndex(i int) (*Node, error) {
+    if l.Size() < i || i < 0 {
+        return nil, errors.New("The index must be between 0 and the list size")
+    }
+
+    if l.IsEmpty() {
+        return nil, errors.New("The list is empty")
+    }
+    
+    curr := l.Head
+    count := 0
+    // TODO - Analyze this condition
+    for curr != nil {
+        count++
+        if count == i {
+            break
+        }
+
+        curr = curr.Next
+    }
+
+    return curr, nil
+}
+
+func (l *List) Size() (int) {
+    size := 0
+    curr := l.Head
+
+    if !l.IsEmpty() {
+        for curr.Next != nil {
+            size++
+            curr = curr.Next
+        }
+    }
+
+    return size
+}
+
+func (l *List) Contains(value int) {
+	curr := l.Head
+	for curr != nil {
+		if curr.Value == value {
+			fmt.Println(true)
+			return
+		}
+
+		curr = curr.Next
+	}
+
+	fmt.Println(false)
+	return
+}
+
+func (l *List) PrintList() {
+	curr := l.Head
+	for curr != nil {
+		fmt.Printf("%d\n", curr.Value)
+		curr = curr.Next
+	}
+}
